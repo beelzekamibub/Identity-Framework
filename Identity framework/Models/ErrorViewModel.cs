76e21722ba3f70639fb179247b0680e20d6a2e63
Chaitanya Sharma <chait8126@gmail.com>
2023-10-13 23:32:17 +0530

    init

---
+++ Identity framework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Identity_framework.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
